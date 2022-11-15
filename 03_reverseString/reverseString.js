2a5fefd9c7a4ba988b9e8c8f7dbf608516bc8acd
Joe Seger <joesegerdev@gmail.com>
2022-11-15 15:52:35 -0500

    Enable all tests and add solution to exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    const stringArray = text.split("");
+    stringArray.reverse();
+    let reversedText = ''
+    for (letter of stringArray) {
+        reversedText += letter;
+    };
 
+    return reversedText;
 };
 
 // Do not edit below this line
