3bdb131695b689f5d5d8361406c36f6acab88849
Joe Seger <joesegerdev@gmail.com>
2022-11-30 15:42:31 -0500

    Modify function to pass remaining test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,20 @@
 const sumAll = function() {
     
+    const args = Array.from(arguments)
+
+    for (let i = 0; i < args.length; i++){
+        if (typeof args[i] != "number" || args[i] < 0){            
+            return("ERROR");
+        }
+
+    }
+
+    let large = Math.max(args[0], args[1]);
+    let small = Math.min(args[0], args[1]);
+
     let total = 0;
     
-    for (let i = arguments[0]; i <= arguments[1]; i++){
+    for (let i = small; i <= large; i++){
         total += i
     }
 
