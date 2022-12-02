834338a093c31ec73ec3bf2fbadcf418b467e2c6
Joe Seger <joesegerdev@gmail.com>
2022-12-02 15:10:48 -0500

    Modify ftoc function to pass base test case

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,10 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  
+  return Math.round((temp - 32) * (5/9) * 10) / 10
 
 };
 
-const ctof = function() {
+const ctof = function(temp) {
 
 };
 
